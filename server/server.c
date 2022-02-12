dc832f5abbbbd0103e0efe485c490802389b21c2
N3cr0 <19351579+Necronhym@users.noreply.github.com>
2022-02-13 00:06:33 +0100

    Initial server comit

---
+++ server/server.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define MAX_LINE 4096
+#define SRVR_PORT 80
+
+int main(int argc, char** argv)
+	{
+		int listenfd, connfd, n;
+		struct sockaddr_in servaddr;
+		uint8_t		buff[MAX_LINE+1];
+		uint8_t		recvLine[MAX_LINE+1];
+		listenfd = socket(AF_INET, SOCK_STREAM, 0);
+		bzero(&servaddr, sizeof(servaddr));
+		servaddr.sin_family = AF_INET;
+		servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+		servaddr.sin_port = htons(SRVR_PORT);
+		bind(listenfd, (struct sockaddr*) &servaddr, sizeof(servaddr));
+		listen(listenfd, 10);
+		while(1)
+			{
+				struct sockaddr_in addr;
+				socklen_t addr_len;
+				fflush(stdout);
+				connfd = accept(listenfd, (struct sockaddr*) NULL, NULL);
+				memset(recvLine, 0, MAX_LINE);
+				while( (n = read(connfd, recvLine, MAX_LINE-1)) > 0 )
+					{
+						fprintf(stdout, "%s\n", recvLine);
+						if(recvLine[n-1] == '\n')
+							{
+								break;
+							}
+					}
+				memset(recvLine, 0, MAX_LINE);
+				snprintf((char*)buff, sizeof(buff),
+					       	"HTTP/1.0 200 OK\r\n\r\n <html> <h1>Hello Web</h1> </html>");
+				write(connfd, (char*)buff, strlen((char*)buff));
+				close(connfd);
+			}
+
+
+		return 0;
+	}
